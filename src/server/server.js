27fb2c0daf119248363f490168ba34597ac82f98
Ulises Martínez Elías <ulises.martinez@dicex.com>
2025-10-14 00:01:01 -0600

    Config ip address

--- src/server/server.js
+++ src/server/server.js
@@ -27,8 +27,8 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar servidor
-app.listen(PORT, () => {
-    console.log(`Servidor de streaming ejecutándose en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor de streaming ejecutándose en el puerto${PORT}`);
     console.log(`Directorio de videos: ${config.videosPath}`);
 });
 
