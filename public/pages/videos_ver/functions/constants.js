8de9e1dae5983235d3ff2ce23ce8a3cee092f5e6
Ulises Martínez Elías <ulises.martinez@dicex.com>
2025-10-23 07:11:14 -0600

    Diseño
    Se añadió nuevo diseño en todos los módulos

--- public/pages/videos_ver/functions/constants.js
+++ public/pages/videos_ver/functions/constants.js
@@ -1,7 +1,7 @@
 let urlApi = '';
 
 if(window.location.hostname === 'localhost')
-    urlApi = 'http://189.157.205.246:3000';
+    urlApi = 'http://189.157.193.110:3000';
 
 const urlVideosVer = `${urlApi}/api/v1/videos`;
 const urlVideosViews = `${urlApi}/api/v1/videos`;
