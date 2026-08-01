fa0544f29e4603a474bce9a5757bb04830cdc80d
Ulises Martínez Elías <ulises.martinez@dicex.com>
2026-08-01 01:19:40 -0600

    Added video type at playing video

--- public/pages/videos_ver_v2/functions/fun.videos_ver.js
+++ public/pages/videos_ver_v2/functions/fun.videos_ver.js
@@ -54,7 +54,7 @@ const funCargarVideo = async () => {
 
         
         //Gb.getComponent('video_player').setData(videoData, url_hls_base);
-        if(videoData.vid_path.includes('https://')) {
+        if(videoData.vid_tipo == 2 || videoData.vid_path.includes('https://')) {
             Gb.getComponent('video_player').setData(videoData);
             Gb.getComponent('video_player').setSrc(videoData.vid_path);
         } else {
